61edd9e0cc2cb4b4cfd06dc0c9b0947f91fe7266
Miriam Cordes <miriam.cordes@intive-kupferwerk.com>
2022-12-10 19:30:13 +0100

    add comments to app.js

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -18,63 +18,58 @@
  * Great to have comments before crucial code sections within the procedure.
 */
 
-/**
- * Define Global Variables
- * 
-*/
-
 const paragraph1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.";
 const paragraph2 = "Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.";
 const paragraphList = [paragraph1, paragraph2]
 
 const mainContent = document.querySelector('main');
 
+// class to hold the section data
 class Section {
-    constructor(id, dataNav, isActive, header, paragraphs) {
+    constructor(id, dataNav, header, paragraphs) {
         this.id = id;
         this.dataNav = dataNav;
-        this.isActive = isActive;
         this.header = header;
         this.paragraphs = paragraphs;
     }
 }
 
+// list of sections
 const sections = [
-    new Section(id = "section1", dataNav = "Section 1", isActive = true, header = "Section 1", paragraphs = paragraphList),
-    new Section(id = "section2", dataNav = "Section 2", isActive = false, header = "Section 2", paragraphs = paragraphList),
-    new Section(id = "section3", dataNav = "Section 3", isActive = false, header = "Section 3", paragraphs = paragraphList),
-    new Section(id = "section4", dataNav = "Section 4", isActive = false, header = "Section 4", paragraphs = paragraphList),
-    new Section(id = "section5", dataNav = "Section 5", isActive = false, header = "Section 5", paragraphs = paragraphList)
+    new Section(id = "section1", dataNav = "Section 1", header = "Section 1", paragraphs = paragraphList),
+    new Section(id = "section2", dataNav = "Section 2", header = "Section 2", paragraphs = paragraphList),
+    new Section(id = "section3", dataNav = "Section 3", header = "Section 3", paragraphs = paragraphList),
+    new Section(id = "section4", dataNav = "Section 4", header = "Section 4", paragraphs = paragraphList),
+    new Section(id = "section5", dataNav = "Section 5", header = "Section 5", paragraphs = paragraphList)
 ];
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
+// create section element
 function buildSectionElement(section) {
     const sectionElement = document.createElement('section');
     sectionElement.id = section.id;
     sectionElement.dataNav = section.dataNav;
-    if (section.isActive) {
-        sectionElement.className = "your-active-class";
+    // initially set the first section as active
+    if (section.id == "section1") {
+        sectionElement.classList.add("your-active-class");
     }
     return sectionElement;
 }
 
+// create wrapper div element
 function buildWrapperElement() {
     const wrapperElement = document.createElement('div');
     wrapperElement.className = "landing__container";
     return wrapperElement;
 }
 
+// create header element
 function buildHeaderElement(headerText) {
     const headerElement = document.createElement('h2');
     headerElement.textContent = headerText;
     return headerElement;
 }
 
+// create paragraph elements
 function buildParagraphElements(paragraphs) {
     const paragraphFragment = document.createDocumentFragment();
     for (const paragraph of paragraphs) {
@@ -85,6 +80,7 @@ function buildParagraphElements(paragraphs) {
     return paragraphFragment;
 }
 
+// build html elements and add them to the html
 function buildSections() {
     const sectionFragment = document.createDocumentFragment();
     for (const section of sections) {
@@ -100,15 +96,17 @@ function buildSections() {
     mainContent.append(sectionFragment);
 }
 
+// build navbar elements
 function buildNavbarElement(section) {
     const navbarElement = document.createElement('li');
     const anchorElement = document.createElement('a');
     anchorElement.className = "menu__link"
     anchorElement.textContent = section.header;
-    navbarElement.appendChild(anchorElement);
+    navbarElement.append(anchorElement);
     return navbarElement;
 }
 
+// build navbar elements and add them to the html
 function buildNavbar() {
     const navbar = document.getElementById('navbar__list');
     const navbarFragment = document.createDocumentFragment();
@@ -119,18 +117,22 @@ function buildNavbar() {
     navbar.append(navbarFragment);
 }
 
+// handle click on navbar elemennt
 function onNavbarElementClicked(event) {
     event.preventDefault();
     const sectionToScrollTo = sections.find(function (section) {
         return section.dataNav == event.target.textContent;
     })
+    // scroll to anchor ID using scrollTO event
     const sectionElementToScrollTo = document.getElementById(sectionToScrollTo.id);
     sectionElementToScrollTo.scrollIntoView({ behavior: "smooth" });
 }
 
+// highlight navbar element corresponding to active section
 function highlightNavBarElement() {
     const navbarElements = document.getElementsByTagName('a');
-    const activeSectionElement = document.querySelector('.your-active-class')
+    const activeSectionElement = document.querySelector(".your-active-class")
+    if (activeSectionElement == null) return;
     for (navbarElement of navbarElements) {
         if (navbarElement.parentElement.nodeName != "LI") {
             continue;
@@ -143,10 +145,12 @@ function highlightNavBarElement() {
     }
 }
 
+// handle document scrolled
 function onDocumentScrolled(event) {
     const sectionElements = document.getElementsByTagName('section');
     for (const sectionElement of sectionElements) {
         const box = sectionElement.getBoundingClientRect();
+        // add class 'active' to section when near top of viewport
         if (box.top <= 150 && box.bottom >= 150) {
             sectionElement.classList.add("your-active-class");
         } else {
@@ -156,6 +160,7 @@ function onDocumentScrolled(event) {
     highlightNavBarElement();
 }
 
+// set up event listeners for document scroll and navbar item click
 function setUpEventListeners() {
     const navbarElements = document.getElementsByTagName('li');
     for (const navbarElement of navbarElements) {
@@ -167,34 +172,5 @@ function setUpEventListeners() {
 buildSections();
 buildNavbar();
 setUpEventListeners();
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- *
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- *
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
 
 
